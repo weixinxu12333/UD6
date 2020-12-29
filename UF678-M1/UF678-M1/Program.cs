5a866a6b32504af7b07eb37e1375afb702e52383
Weixin <weixin.xu.2018@gmail.com>
2020-12-30 00:31:02 +0100

    EX1-M1-FASE1 ACABADO

---
+++ UF678-M1/UF678-M1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace UF678_M1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Milstone1");
+            Milstone1 m1 = new Milstone1();
+            m1.inicio();
+        }
+    }
+}

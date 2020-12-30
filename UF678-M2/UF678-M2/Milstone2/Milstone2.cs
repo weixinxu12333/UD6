b076e6d3898ffeccd5ac956736e13d9748ba2cdb
Weixin <weixin.xu.2018@gmail.com>
2020-12-30 21:35:51 +0100

    EX2-M2-FASE 2 ACABADO

---
+++ UF678-M2/UF678-M2/Milstone2/Milstone2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UF678_M2
+{
+    class Milstone2
+    {
+        public void Inicio()
+        {
+            Console.WriteLine("Escriu un nombre:");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(j);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
--- UF678-M2/UF678-M2/Program.cs
+++ UF678-M2/UF678-M2/Program.cs
@@ -10,6 +10,9 @@ namespace UF678_M2
         {
             Milstone1 m1 = new Milstone1();
             m1.Inicio();
+
+            Milstone2 m2 = new Milstone2();
+            m2.Inicio();
         }
     }
 }

f50b808fc9d04b14a4b23e50237ca9d4f3634615
Weixin <weixin.xu.2018@gmail.com>
2021-01-01 18:03:59 +0100

    EX3-M3-FASE 1 ACABADO

---
+++ UF678-M3/UF678-M3/Milstone1/Milstone1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UF678_M3
+{
+    class Milstone1
+    {
+        public void Inici()
+        {
+            string c1, c2, c3, c4, c5, c6;
+            c1 = EscriuNomCiutat();
+            c2 = EscriuNomCiutat();
+            c3 = EscriuNomCiutat();
+            c4 = EscriuNomCiutat();
+            c5 = EscriuNomCiutat();
+            c6 = EscriuNomCiutat();
+
+            ImprimirNomCiutat(c1);
+            ImprimirNomCiutat(c2);
+            ImprimirNomCiutat(c3);
+            ImprimirNomCiutat(c4);
+            ImprimirNomCiutat(c5);
+            ImprimirNomCiutat(c6);
+        }
+
+        public static string EscriuNomCiutat()
+        {
+            Console.WriteLine("Escriu nom de un ciutat: ");
+            string ciutat = Console.ReadLine();
+            return ciutat;
+        }
+
+        public static void ImprimirNomCiutat(string ciutat)
+        {
+            Console.WriteLine(ciutat);
+        }
+    }
+
+
+}
---
+++ UF678-M3/UF678-M3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace UF678_M3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Milstone1 m1 = new Milstone1();
+            m1.Inici();
+        }
+
+        
+    }
+}

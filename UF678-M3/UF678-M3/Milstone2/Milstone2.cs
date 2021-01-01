4b486696abf7810f5fc0a01366e3b07f26dee4e5
Weixin <weixin.xu.2018@gmail.com>
2021-01-01 20:18:45 +0100

    EX3-M3-MILSTONE2 ACABADO

--- UF678-M3/UF678-M3/Milstone2/Milstone2.cs
+++ UF678-M3/UF678-M3/Milstone2/Milstone2.cs
@@ -8,6 +8,29 @@ namespace UF678_M3
     {
         public void Inici()
         {
+            double[,] notes = new double[5,3];
+
+            for (int i = 0; i < notes.GetLength(0); i++)
+            {
+                Console.WriteLine("Introduir notes de l'alumne :" + i);
+                for (int j = 0; j < notes.GetLength(1); j++)
+                {
+                    Console.WriteLine("Introduir nota {0} de l'alumne {1}", j, i);
+                    notes[i,j] = Convert.ToDouble(Console.ReadLine());
+                }
+            }
+
+            for (int i = 0; i < notes.GetLength(0); i++)
+            {
+                double suma = 0;
+                for (int j = 0; j < notes.GetLength(1); j++)
+                {
+                    suma += notes[i, j];
+                }
+                double mitjana = suma / notes.GetLength(1);
+                Console.WriteLine("Mitja de l'alumne {0}: {1} L'alumne ha {2}", i, mitjana, mitjana >= 5 ? "aprovat" : "suspes" );
+            }
+
         }
     }
 }
--- UF678-M3/UF678-M3/Program.cs
+++ UF678-M3/UF678-M3/Program.cs
@@ -6,8 +6,8 @@ namespace UF678_M3
     {
         static void Main(string[] args)
         {
-            Milstone1 m1 = new Milstone1();
-            m1.Inici();
+            //Milstone1 m1 = new Milstone1();
+            //m1.Inici();
 
             Milstone2 m2 = new Milstone2();
             m2.Inici();
